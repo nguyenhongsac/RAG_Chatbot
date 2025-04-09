1a556ee08a071b0f1268a1427900e3b10603849f
nguyenhongsac <sac.34.10a1@gmail.com>
2025-04-10 01:07:21 +0700

    Testing deepseek-v3 as llm

--- llm.py
+++ llm.py
@@ -46,9 +46,8 @@ rerank_model = MxbaiRerankV2("mixedbread-ai/mxbai-rerank-base-v2")
 '''
 from langchain.prompts import PromptTemplate
 # Define a Vietnamese prompt
-prompt_template = """Bạn là một trợ lý để trả lời câu hỏi. Suy nghĩ dựa trên dữ liệu liên quan, sau đó đưa ra phản hồihồi bằng tiếng Việt. 
-Nếu không biết câu trả lời, chỉ cần nói rằng bạn không biết.
-
+prompt_template = """Bạn là một chatbot hỏi đáp thông minh. Hãy cung cấp câu trả lời chính xác dựa trên dữ liệu liên quan. Nếu bạn không biết câu trả lời, chỉ cần nói không biết. 
+Phản hồi bằng tiếng Việt một cách tự nhiên, trình bày khoa học. Không lặp lại câu hỏi của người dùng. Không bắt đầu bằng "Theo thông tin được cung cấp", "Theo dữ liệu được cung cấp".
 Câu hỏi: {question}
 
 Dữ liệu liên quan:
