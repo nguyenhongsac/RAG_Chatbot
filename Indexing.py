116ba0bdac1471ddc737f17ee05e7eb9d83fd4d8
nguyenhongsac <sac.34.10a1@gmail.com>
2025-04-14 22:31:17 +0700

    add embedding models

--- Indexing.py
+++ Indexing.py
@@ -1,14 +1,30 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import bs4
+from langchain_chroma import Chroma
+
+def indexing(loader, embeddings, stored_path):
+    """
+    """
+    # Vector store
+    vector_store = Chroma(embedding_function=embeddings, persist_directory=stored_path)
+
+    # Loading
+    docs = loader.load()
+
+    # Chunking
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    all_splits = text_splitter.split_documents(docs)
+
+    # Index chunks
+    _ = vector_store.add_documents(documents=all_splits)
 
 # Select embedding models
 from langchain_huggingface import HuggingFaceEmbeddings
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+# embeddings1 = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2") #768 dim
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2") #384 dim
 
-# Vector store
-from langchain_chroma import Chroma
-vector_store = Chroma(embedding_function=embeddings, persist_directory=r"E:\Data\chromadb")
+stored_path = r"E:\Data\chromadb\eb"
 
 # Load and chunk contents of the blog
 loader = WebBaseLoader(
@@ -19,12 +35,7 @@ loader = WebBaseLoader(
     #     )
     # ),
 )
-docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-all_splits = text_splitter.split_documents(docs)
-
-# Index chunks
-_ = vector_store.add_documents(documents=all_splits)
+indexing(loader=loader, embeddings=embeddings, stored_path=stored_path)
 
 print("Ingestion data done!")
