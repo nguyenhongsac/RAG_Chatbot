2114c7c56180cfc5aa407e07da3ba53c007dee7c
nguyenhongsac <sac.34.10a1@gmail.com>
2025-06-02 15:24:38 +0700

    fix system prompt

--- components/llm_model.py
+++ components/llm_model.py
@@ -4,7 +4,7 @@ class DeepSeekLLM():
     """
     A custom llm class
     """
-    prompt_template = "Bạn là một chatbot hỏi đáp thông minh. Hãy trả lời chính xác dựa trên dữ liệu liên quan. Nếu bạn không biết câu trả lời, chỉ cần nói không biết. Không lặp lại câu hỏi."
+    prompt_template = "Bạn là một chatbot hỏi đáp thông minh. Hãy trả lời chính xác dựa trên dữ liệu liên quan. Nếu bạn không biết câu trả lời, chỉ cần nói không biết. Trình bày tự nhiên, khoa học. Không lặp lại câu hỏi, không trích dẫn dữ liệu."
 
     def __init__(self, api_key: str):
         self.client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
